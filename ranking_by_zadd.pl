31bddc8ff15616628476f5ffd7c58e1e12794de0
Keiji, Yoshimi <walf443@gmail.com>
2013-02-24 17:34:57 +0900

    added slave support.

--- ranking_by_zadd.pl
+++ ranking_by_zadd.pl
@@ -21,10 +21,19 @@ while ( $pm->signal_received ne 'TERM' ) {
     $pm->start(sub {
         srand();
         my $redis = RedisDB->new();
+        my $redis_slave;
+        if ( $ENV{USE_SLAVE} ) {
+            $redis_slave = RedisDB->new(host => 'localhost', port => 6380);
+        }
 
         my $random_user_id = int(rand() * 10000);
         my $score = int(rand() * 100) * 10;
-        my $bscore = $redis->zscore("ranking:01", "user_id:$random_user_id") || 0;
+        my $bscore;
+        if ( $redis_slave ) {
+            $bscore = $redis_slave->zscore("ranking:01", "user_id:$random_user_id") || 0;
+        } else {
+            $bscore = $redis->zscore("ranking:01", "user_id:$random_user_id") || 0;
+        }
         $redis->zadd("ranking:01", $score + $bscore, "user_id:$random_user_id");
     });
 }
